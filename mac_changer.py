69334e15ee6355740de6d253e0bf0ad5220a2a86
Chan Lee <chanethanlee@gmail.com>
2018-09-22 21:11:29 -0700

    refactor check output function.

--- mac_changer.py
+++ mac_changer.py
@@ -22,15 +22,16 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return (mac_address_search_result.group(0))
+    else:
+        print("[-] Could not find mac address.")
+
 options = get_arguments()
 change_mac(options.interface, options.new_mac)
-
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_result:
-    print(mac_address_search_result)
-else:
-    print("[-] Could not find mac address.")
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
