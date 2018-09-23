98daabeb9ec06b39d7e465edcc05ca110b643466
Chan Lee <chanethanlee@gmail.com>
2018-09-22 21:58:34 -0700

    fix non-byte regex searching bug and fix typo.

--- mac_changer.py
+++ mac_changer.py
@@ -24,7 +24,7 @@ def change_mac(interface, new_mac):
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
 
     if mac_address_search_result:
         return (mac_address_search_result.group(0))
@@ -37,7 +37,7 @@ print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
 current_mac = get_current_mac(options.interface)
 
-if currnet_mac == options.new_mac:
-    print("[+] MAC address was successfully changed to "+ current_mac)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
 else:
     print("[-] MAC address did not get changed.")
