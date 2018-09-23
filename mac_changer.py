7e5349ea8a8d419d2583332d95987d80af44b6a5
Chan Lee <chanethanlee@gmail.com>
2018-09-22 21:05:17 -0700

    use regex for locate newmac address from the result, apply branching.

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -24,4 +25,12 @@ def change_mac(interface, new_mac):
 options = get_arguments()
 change_mac(options.interface, options.new_mac)
 
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
 
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address_search_result:
+    print(mac_address_search_result)
+else:
+    print("[-] Could not find mac address.")
