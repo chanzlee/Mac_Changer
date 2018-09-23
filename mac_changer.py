e748a86086961a4eb14b51bc3b319ab4b36a80d2
Chan Lee <chanethanlee@gmail.com>
2018-09-22 20:59:52 -0700

    modulize syntax.

--- mac_changer.py
+++ mac_changer.py
@@ -3,21 +3,25 @@
 import subprocess
 import optparse
 
-#create instance
-parser = optparse.OptionParser()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="Interface to change its MAC address")
+    parser.add_option("-m","--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify interface. --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify new mac address. --help for more info.")
+    return options
 
-parser.add_option("-i","--interface",dest="interface",help="Interface to change its MAC address")
-parser.add_option("-m","--mac", dest="new_mac", help="New MAC Address")
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface])
 
-(options, arguments) = parser.parse_args()
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
 
-#set options
-interface = options.interface
-new_mac = options.new_mac
 
-print("[+] Changing MAC address for "+interface+" to "+new_mac)
-
-subprocess.call(["ifconfig" ,interface, "down"])
-subprocess.call(["ifconfig",interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig",interface, "down"])
-subprocess.call(["ifconfig", interface])
